fe99cd4574ba9baaabaf6e24364cebb8ed0ec545
Robab Malik <robab.malik@theowlsense.com>
2022-06-21 02:15:31 +0500

    Modified login elements file for Mobile Automation

--- Task 1 - UI Automation Testing - Mobile/locators/login_elements.py
+++ Task 1 - UI Automation Testing - Mobile/locators/login_elements.py
@@ -5,10 +5,5 @@ class LOGINELEMENTS:
     password_xpath = '/html/body/div[2]/div/div/form/div[2]/div/input'
     login_button_xpath = '/html/body/div[2]/div/div/form/button'
 
-    # locators for elements to verify login was successful
-    logout_button_xpath = '//i[@class="icon-2x icon-signout"]'
-
-    # locators for caputring alert messages / error messages
-    alert_id = 'flash-messages'
     
     
